489a1f9d401a731c79cf9dd4ce81790f64da2a4f
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-09-28 20:21:59 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     header("X-XSS-Protection: 1; mode=block");
     header("X-Content-Type-Options: nosniff");
     header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
-    header("Content-Security-Policy: default-src 'self'");
+    
 ?>
 <script>
  function enableBtn(){
