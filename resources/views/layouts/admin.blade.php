009913792dc1b20ce560201aac9ab2f95cc7798d
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-03 21:02:03 +0800

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,8 +24,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
-    <!-- <link href="{{ asset('css/custom.css') }}" rel="stylesheet" /> -->
-    <link href="https://ae70e3618413.ngrok.io/css/custom.css" rel="stylesheet" />
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+    
     @yield('styles')
 </head>
 
@@ -89,8 +89,8 @@
 
         </main>
        
-        <!-- <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;"> -->
-        <form id="logoutform" action="https://ae70e3618413.ngrok.io/logout" method="POST" style="display: none;">
+        <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
+        
             {{ csrf_field() }}
         </form>
     </div>
