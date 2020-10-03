e0aa369d199b325e6dee8225c574334f52dc3e14
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-03 23:33:33 +0800

    crushed bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
     header("X-Frame-Options: sameorigin");
     header("X-XSS-Protection: 1; mode=block");
     header("X-Content-Type-Options: nosniff");
-    // header("Strict-Transport-Security: max-age=300;");
-    // header("Content-Security-Policy: upgrade-insecure-requests");
+    header("Strict-Transport-Security: max-age=300;");
+    header("Content-Security-Policy: upgrade-insecure-requests");
         
 ?>
 <script>
