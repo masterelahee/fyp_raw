fee7399f99aa0d88318c6a2885e021aada673ee8
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-03 22:09:19 +0800

    Update index.blade.php

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -3,7 +3,7 @@
 @can('appointment_create')
     <div style="margin-bottom: 10px;" class="row">
         <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.appointments.create") }}">
+            <a class="btn btn-success" href="{{ route(admin.appointments.create) }}">
                 {{ trans('global.add') }} {{ trans('cruds.appointment.title_singular') }}
             </a>
         </div>
