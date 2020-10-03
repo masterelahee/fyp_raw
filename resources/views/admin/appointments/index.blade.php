63c07ec5b5ce2a709be2277a78fcca2c5bf4fed1
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-03 22:21:54 +0800

    Update index.blade.php

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -95,7 +95,7 @@
   let dtOverrideGlobals = {
     buttons: dtButtons,
     processing: true,
-    // serverSide: true,
+    serverSide: true,
     retrieve: true,
     aaSorting: [],
     ajax: "{{ route('admin.appointments.index') }}",
