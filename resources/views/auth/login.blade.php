d0082c6f1f00be12ef8a0590df82575e53f46e92
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-03 19:35:24 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     header("X-Frame-Options: sameorigin");
     header("X-XSS-Protection: 1; mode=block");
     header("X-Content-Type-Options: nosniff");
-    header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+    header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
         
 ?>
 <script>
