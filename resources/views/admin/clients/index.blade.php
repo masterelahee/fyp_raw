5406655693071980894beea0f38542aebd23c04c
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-04 22:57:14 +0800

    Crushed many bugs

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.admin')
 @section('content')
 @can('client_create')
+<?php
+header("Content-Security-Policy: upgrade-insecure-requests");
+?>
     <div style="margin-bottom: 10px;" class="row">
         <div class="col-lg-12">
             <a class="btn btn-success" href="{{ route("admin.clients.create") }}">
