ec9b32158e9c71ecea62743572ba4f65b2b08c34
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-04 22:32:52 +0800

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,4 +38,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function loggedOut(Request $request)
+    {
+        return redirect('/login');
+    }
 }
