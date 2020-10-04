a0714522508283d195c85dcf0231d251314f9b71
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-04 22:37:19 +0800

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -39,5 +40,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/login');
+      }
     
 }
