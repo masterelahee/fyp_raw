c46a05bd0a955a6d3538e5b72f0ca1f1fbc8d69b
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-05 12:10:26 +0800

    Update create.blade.php

--- resources/views/admin/appointments/create.blade.php
+++ resources/views/admin/appointments/create.blade.php
@@ -5,7 +5,7 @@ function test() {
     $comments="<script>alert('asda')</script>";
     $comments = filter_var($comments, FILTER_SANITIZE_STRING);
     $comments = filter_var($comments, FILTER_SANITIZE_SPECIAL_CHARS);
-    return '<textarea id="comments" name="comments" class="form-control ">$comments</textarea>';
+    return '<textarea id="comments" name="comments" class="form-control ">'+$comments+'</textarea>';
 }
 ?>
 <div class="card">
