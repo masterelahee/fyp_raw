187b32b620089e0d0aecb80e02e186e032a1e077
Irfan <48475261+masterelahee@users.noreply.github.com>
2020-10-05 15:52:23 +0800

    Update create.blade.php

--- resources/views/admin/appointments/create.blade.php
+++ resources/views/admin/appointments/create.blade.php
@@ -73,7 +73,7 @@
             </div>
             <div class="form-group {{ $errors->has('comments') ? 'has-error' : '' }}">
                 <label for="comments">{{ trans('cruds.appointment.fields.comments') }}</label>
-                 <textarea id="comments" name="comments" class="form-control ">{!! old('comments', isset($appointment) ? $appointment->comments : '') !!}</textarea> 
+                 <textarea id="comments" name="comments" class="form-control ">{{ old('comments', isset($appointment) ? $appointment->comments : '') }}</textarea> 
                 
                 @if($errors->has('comments'))
                     <em class="invalid-feedback">
