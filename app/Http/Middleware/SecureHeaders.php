7948e6d1f7c9c364d3c91746ea05dcdd2d28ee05
masterelahee <48475261+masterelahee@users.noreply.github.com>
2020-11-01 22:35:24 +0800

    Update SecureHeaders.php

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -19,8 +19,8 @@ class SecureHeaders
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
-        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-        $response->headers->set('Content-Security-Policy', "style-src 'self'"); // Clearly, you will be more elaborate here.
+        $response->headers->set('Content-Security-Policy', "script-src 'self'", "connect-src 'self'", "img-src 'self'");
+	$response->headers->set('Permissions-Policy', 'geolocation=()');
         return $response;
     }
     private function removeUnwantedHeaders($headerList)
